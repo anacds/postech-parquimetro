38fe51bac42a971ca3f8457b6c29bfa706e42a72
moreiralud <ludmila.brmr@gmail.com>
2025-01-18 19:31:28 -0300

    adiciona swagger aos endpoints de regiao

--- parquimetro/src/main/java/com/fiap/parquimetro/controller/RegiaoController.java
+++ parquimetro/src/main/java/com/fiap/parquimetro/controller/RegiaoController.java
@@ -2,6 +2,8 @@ package com.fiap.parquimetro.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,38 +21,45 @@ import com.fiap.parquimetro.service.RegiaoService;
 
 @RestController
 @RequestMapping(value = "regioes")
+@Tag(name = "Regiões", description = "Gerenciamento de regiões do sistema.")
 public class RegiaoController {
 
 	@Autowired
 	private RegiaoService regiaoService;
-	
+
 	@GetMapping
-	private List<Regiao> obterTodos(){
+  @Operation(summary = "Listar todas as regiões", description = "Retorna uma lista com todas as regiões cadastradas no sistema.")
+  private List<Regiao> obterTodos(){
 		return regiaoService.obterTodos();
 	}
-	
+
 	@GetMapping("/zona")
-	private List<Regiao> obterPorZona(@RequestParam String zona){
+  @Operation(summary = "Obter regiões por zona", description = "Retorna uma lista de regiões filtradas com base na zona fornecida.")
+  private List<Regiao> obterPorZona(@RequestParam String zona){
 		return regiaoService.obterPorZona(zona);
 	}
-	
+
 	@GetMapping("{id}")
-	private Regiao obterPorId(@PathVariable String id) {
+  @Operation(summary = "Obter região por ID", description = "Retorna os detalhes de uma região específica com base no ID fornecido.")
+  private Regiao obterPorId(@PathVariable String id) {
 		return regiaoService.obterPorId(id);
 	}
-	
+
 	@DeleteMapping("{id}")
-	private void excluirVaga(@PathVariable String id) {
+  @Operation(summary = "Excluir uma vaga", description = "Remove uma vaga do sistema com base no ID fornecido.")
+  private void excluirVaga(@PathVariable String id) {
 		regiaoService.excluirRegiao(id);
 	}
-	
+
 	@PostMapping
-	private Regiao criar(@RequestBody Regiao regiao) {
+  @Operation(summary = "Criar uma nova região", description = "Cadastra uma nova região no sistema com as informações fornecidas.")
+  private Regiao criar(@RequestBody Regiao regiao) {
 		return regiaoService.criar(regiao);
 	}
-	
+
 	@PutMapping("{id}")
-	private ResponseEntity<?> alterarRegiao(@PathVariable String id, @RequestBody Regiao regiao){
+  @Operation(summary = "Alterar uma região", description = "Altera os dados de uma região existente com base no ID fornecido e nas novas informações.")
+  private ResponseEntity<?> alterarRegiao(@PathVariable String id, @RequestBody Regiao regiao){
 		return regiaoService.alterarRegiao(id, regiao);
 	}
 }
